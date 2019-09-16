9474c1280b033a9ea0382f548ecd84e1386a99e9
Mick42 <mickael.wala@gmail.com>
2019-09-17 00:29:40 +0200

    maj style panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+    
 /**
  * @Route("/panier")
  */
