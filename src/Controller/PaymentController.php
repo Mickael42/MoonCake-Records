90a7ebae387092155e724129cb38d6d5e3731602
Mick42 <mickael.wala@gmail.com>
2019-09-28 19:43:59 +0200

    debugs-recettage

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -21,7 +21,6 @@ class PaymentController extends AbstractController
         $response = new Response();
         $response->headers->clearCookie("id");
 
-
         $amount = $order->getTotalAmount() * 100;
 
         \Stripe\Stripe::setApiKey('sk_test_bfHv9DFUb0tj17UHOsD63dks00IUrgmQ30');
@@ -32,13 +31,25 @@ class PaymentController extends AbstractController
             'payment_method_types' => ['card'],
             'metadata' => ['order_id' => $order->getId()],
         ]);
-        
-             $charges = \Stripe\Charge::all([
+
+        $charges = \Stripe\Charge::all([
             'payment_intent' => '{{PAYMENT_INTENT_ID}}',
             // Limit the number of objects to return (the default is 10)
             'limit' => 3,
-             ]);
-    
+        ]);
+
+        $checkbox = $request->request->get('check');
+        $nameCartHolder = $request->request->get('cartHolder');
+
+         if (!empty($checkbox) and !empty($nameCartHolder)) {
+            return $this->redirectToRoute('payment_confirmation', [
+                'id' => $order->getId()
+            ]);
+        } 
+
+
+
+
         return $this->render('payment/index.html.twig', [
             'controller_name' => 'PaymentController',
             'order' => $order,
@@ -51,5 +62,7 @@ class PaymentController extends AbstractController
      * @Route("/confirmation/{id}", name="payment_confirmation", methods={"POST", "GET"})
      */
     public function paymentConfirmation(Orders $order, Request $request)
-    { }
+    {
+        return $this->render('payment/confirmation.html.twig');
+    }
 }
