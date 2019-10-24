de9249616eef1126ff8e9638c22eddb7072ef7f6
Mick42 <mickael.wala@gmail.com>
2019-10-25 00:47:03 +0200

    ajout confirmation mail

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Orders;
 use App\Entity\Vinyl;
+use App\Manager\EmailManager;
 use App\Manager\SessionManager;
 use App\Repository\OrderProductRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,7 +18,7 @@ class PaymentController extends AbstractController
      */
     public function paymentForm(Orders $order, Request $request)
     {
-       
+
         $amount = $order->getTotalAmount() * 100;
 
         \Stripe\Stripe::setApiKey('sk_test_bfHv9DFUb0tj17UHOsD63dks00IUrgmQ30');
@@ -34,35 +35,32 @@ class PaymentController extends AbstractController
         $paymentIntent = $request->request->get('paymentIntent');
 
 
- if (!empty($checkbox) and !empty($nameCartHolder) and !empty($paymentIntent)) {
+        if (!empty($checkbox) and !empty($nameCartHolder) and !empty($paymentIntent)) {
 
             \Stripe\Stripe::setApiKey('sk_test_bfHv9DFUb0tj17UHOsD63dks00IUrgmQ30');
 
             $intent = \Stripe\PaymentIntent::retrieve($paymentIntent);
             $charges = $intent->charges->data;
 
-           if($charges[0]->status === "succeeded"){
-            $entityManager = $this->getDoctrine()->getManager();
-            $order->setStatus("paid");
-            $entityManager->persist($order);
-            $entityManager->flush();
+            if ($charges[0]->status === "succeeded") {
 
-            return $this->redirectToRoute('payment_confirmation', [
-                'id' => $order->getId()
-            ]);
-           } else {
 
-            $this->addFlash(
-                'error',
-                'Il y a eu un problème avec le paiement. Veuillez réessayer.'
-            );
-            return $this->render('payment/index.html.twig', [
-                'controller_name' => 'PaymentController',
-                'order' => $order,
-                'clientPayment' => $intent->client_secret
-            ]);
-           }         
- }
+                return $this->redirectToRoute('payment_confirmation', [
+                    'id' => $order->getId()
+                ]);
+            } else {
+
+                $this->addFlash(
+                    'error',
+                    'Il y a eu un problème avec le paiement. Veuillez réessayer.'
+                );
+                return $this->render('payment/index.html.twig', [
+                    'controller_name' => 'PaymentController',
+                    'order' => $order,
+                    'clientPayment' => $intent->client_secret
+                ]);
+            }
+        }
         return $this->render('payment/index.html.twig', [
             'controller_name' => 'PaymentController',
             'order' => $order,
@@ -73,10 +71,12 @@ class PaymentController extends AbstractController
     /**
      * @Route("/confirmation/{id}", name="payment_confirmation", methods={"POST", "GET"})
      */
-    public function paymentConfirmation(Orders $order, OrderProductRepository $orderProductRepository, SessionManager $sessionManager)
+    public function paymentConfirmation(Orders $order, OrderProductRepository $orderProductRepository, SessionManager $sessionManager, \Swift_Mailer $mailer)
     {
-        $cart = $order->getCart();
         $entityManager = $this->getDoctrine()->getManager();
+        $cart = $order->getCart();
+        $order->setStatus("paid");
+        $entityManager->persist($order);
 
         //Deleting in the database all products selected and link to the cart
         //also updating the available quantity stock for each purchased vinyl 
@@ -95,6 +95,25 @@ class PaymentController extends AbstractController
         $entityManager->flush();
         //Deleteing the cart stored in the session
         $sessionManager->delete();
+
+        //create the email with all the variables about the customer
+        
+        $message = (new \Swift_Message('Confirmation de commande | MoonCake Records'))
+            ->setFrom('contact@mooncakerecords.com')
+            ->setTo($order->getClient()->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'email/confirmation.html.twig',
+                    [   
+                        'name' => $order->getClient()->getFirstname(),
+                        'date'=>$order->getOrderDate(),
+                        'orderId'=>$order->getId(),
+                        'totalAmount' => $order->getTotalAmount()
+                    ]
+                ),
+                'text/html'
+            );  
+        $mailer->send($message);
 
 
         return $this->render('payment/confirmation.html.twig');
